4677394c14990d11361cd37d4925380ee27b6562
Ksenia <krylovkseniya39@gmail.com>
2021-09-29 13:20:41 +0400

    create: added slider in hotel section

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button_next',
+      prevEl: '.slider-button_prev',
+    },
+    effect: "fade",
+  });
