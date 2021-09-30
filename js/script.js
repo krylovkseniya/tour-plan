b6701f7c4e728e7cde8661d37e1f5944b12b9044
Ksenia <krylovkseniya39@gmail.com>
2021-09-30 22:08:50 +0400

    create: section reviews

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
-    loop: true,
+  loop: true,
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider-button_next',
-      prevEl: '.slider-button_prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button_next',
+    prevEl: '.slider-button_prev',
+  },
 
-    effect: "fade",
+  effect: "fade",
 
-    keyboard: {
-        enabled: true,
-    },
-  });
+  keyboard: {
+      enabled: true,
+  },
+});
+
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button_next',
+    prevEl: '.reviews-slider__button_prev',
+  },
+
+  keyboard: {
+      enabled: true,
+  },
+});
